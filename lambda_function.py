34e0eaf9aa85730db0ca4b8b649978aee5c81477
Taslosis <alvinwu9@gmail.com>
2023-11-07 23:45:03 -0800

    Added increment for health to test

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,22 @@
 import logging
 import json
+import boto3
 from customEncoder import CustomEncoder
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+dynamoName = "extracta-data"
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamoName)
+
 getMethod = "GET"
+postMethod = "POST"
+
 dataPath = "/data"
+rekogPath = dataPath + "/rekognition"
+comprehendPath = dataPath + "/comprehend"
+transcribePath = dataPath + "/transcribe"
 healthPath = dataPath + "/health"
 
 #main handler
@@ -17,6 +27,12 @@ def lambda_handler(event, context):
 
     if httpMethod == getMethod and path == healthPath:
         response = buildResponse(200)
+    elif httpMethod == getMethod and path == rekogPath:
+        response = rekogResponse(200)
+    elif httpMethod == getMethod and path == comprehendPath:
+        response = comprehendResponse(200)
+    elif httpMethod == getMethod and path == transcribePath:
+        response = transcribeResponse(200) 
     else:
         response = buildResponse(404, 'Not Found')
     
@@ -24,6 +40,45 @@ def lambda_handler(event, context):
 
 # Response builder.
 def buildResponse(statusCode, body = None):
+    id = 'health'
+    increment_value = 1
+
+    # Update the numericAttribute using an update expression.
+    response = table.update_item(
+        Key={'id': id},
+        UpdateExpression='SET numericAttribute = numericAttribute + :val',
+        ExpressionAttributeValues={':val': increment_value}
+    )
+    
+    if body is not None:
+        response['body'] = json.dumps(body, cls = CustomEncoder)
+    return response
+
+def rekogResponse(statusCode, body = None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body, cls = CustomEncoder)
+    return response
+
+def comprehendResponse(statusCode, body = None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body, cls = CustomEncoder)
+    return response
+
+def transcribeResponse(statusCode, body = None):
     response = {
         'statusCode': statusCode,
         'headers': {
