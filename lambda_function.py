ba9ed75b29071af481851702d39634caffc10835
Taslosis <alvinwu9@gmail.com>
2023-11-13 14:50:49 -0800

    Updated getitem and newclicks

--- lambda_function.py
+++ lambda_function.py
@@ -96,10 +96,15 @@ def transcribeResponse(statusCode, body = None):
 def healthResponse(statusCode, body=None):
     item_id = "health"
 
+    dynamo_response = table.get_item(Key ={'id':item_id})
+    current_clicks = dynamo_response.get('Item', {}).get('clicks',0)
+
+    new_clicks = current_clicks + 1
+
     try:
         update_response = table.put_item(
             TableName=dynamoName,
-            Item={'id': item_id, 'clicks': 1}
+            Item={'id': item_id, 'clicks': new_clicks}
         )
 
         response = {
