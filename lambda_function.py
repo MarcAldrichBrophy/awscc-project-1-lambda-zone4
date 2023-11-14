bb850c4e3a438875e337b0a9f1b918528e08cf6a
Taslosis <alvinwu9@gmail.com>
2023-11-14 03:06:19 -0800

    comprehend update

--- lambda_function.py
+++ lambda_function.py
@@ -22,27 +22,22 @@ healthPath = dataPath + "/health"
 
 #main handler
 def lambda_handler(event, context):
-    try:
-        logger.info(event)
-        httpMethod = event['httpMethod']
-        path = event['path']
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    path = event['path']
 
-        if httpMethod == getMethod and path == healthPath:
-            response = healthResponse(200)
-        elif httpMethod == getMethod and path == rekogPath:
-            response = rekogResponse(200)
-        elif httpMethod == getMethod and path == comprehendPath:
-            response = comprehendResponse(200)
-        elif httpMethod == getMethod and path == transcribePath:
-            response = transcribeResponse(200) 
-        else:
-            response = buildResponse(404, 'Not Found')
-    
-        return response
-    except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        traceback.print_exc()
-        return buildResponse(500, 'Internal Server Error')
+    if httpMethod == getMethod and path == healthPath:
+        response = healthResponse(200)
+    elif httpMethod == getMethod and path == rekogPath:
+        response = rekogResponse(200)
+    elif httpMethod == getMethod and path == comprehendPath:
+        response = comprehendResponse(200)
+    elif httpMethod == getMethod and path == transcribePath:
+        response = transcribeResponse(200) 
+    else:
+        response = buildResponse(404, 'Not Found')
+
+    return response
 
 # Response builder.
 def buildResponse(statusCode, body = None):
@@ -69,17 +64,37 @@ def rekogResponse(statusCode, body = None):
         response['body'] = json.dumps(body, cls = CustomEncoder)
     return response
 
-def comprehendResponse(statusCode, body = None):
-    response = {
-        'statusCode': statusCode,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+def comprehendResponse(statusCode, body=None):
+    item_id = "comprehend"
+
+    dynamo_response = table.get_item(Key={'id': item_id})
+    current_clicks = dynamo_response.get('Item', {}).get('clicks', 0)
+
+    new_clicks = current_clicks + 1
+
+    try:
+        update_response = table.put_item(
+            TableName=dynamoName,
+            Item={'id': item_id, 'clicks': new_clicks}
+        )
+
+        response = {
+            'statusCode': statusCode,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
         }
-    }
-    if body is not None:
-        response['body'] = json.dumps(body, cls = CustomEncoder)
-    return response
+
+        if body is not None:
+            response['body'] = json.dumps(body, cls=CustomEncoder)
+
+        return response
+
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        traceback.print_exc()
+        return buildResponse(500, f'Internal Server Error: {str(e)}')
 
 def transcribeResponse(statusCode, body = None):
     item_id = "transcribe"
